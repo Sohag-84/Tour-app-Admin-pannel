836709fdc551edd2aa2cd57f32887f32ec67be3f
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-12-24 17:14:29 +0600

    set up screen util and getx package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:travel_agency_admin_app/views/home.dart';
 
 import 'Theme/app_theme.dart';
@@ -12,13 +14,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Admin app',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme().lightTheme,
-      darkTheme: AppTheme().darkTheme,
-      themeMode: ThemeMode.system,
-      home: Home(),
+    return ScreenUtilInit(
+      designSize: const Size(428, 926),
+      builder: (BuildContext context,Widget? child) {
+        return GetMaterialApp(
+          title: 'Admin app',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme().lightTheme,
+          darkTheme: AppTheme().darkTheme,
+          themeMode: ThemeMode.system,
+          home: Home(),
+        );
+      },
     );
   }
 }
